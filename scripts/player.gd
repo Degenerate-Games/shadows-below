440b0585c30f6645a66208d2f06e0ef1d706fa00
James Wolff <james.wolff0@gmail.com>
2024-07-21 16:29:03 -0500

    Update player script to extend CharacterBody2D as root node and handle movement and color changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,31 @@
-extends Node2D
+extends CharacterBody2D
 
+@export_category("Player Variables")
+@export var speed: int = 100
+@export var velocity_decay: float = 0.8
+
+var animation_controller: AnimatedSprite2D
+var aura: PointLight2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animation_controller = $AnimatedSprite2D
+	aura = $PointLight2D
 
+func _physics_process(delta):
+	handle_movement(delta)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func handle_movement(delta):
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_dir:
+		velocity = (velocity * 1) + (input_dir * speed * delta)
+		# animation_controller.play("walk")
+		move_and_slide()
+
+func handle_color_change(color):
+	aura.energy = remap(color.r + color.g + color.b, 0.75, 3.0, 4.0, 0.25)
+	aura.color = color
+	print(aura.color, aura.energy)
+
+func _on_color_mixing_ui_color_changed(color):
+	handle_color_change(color)
