3ab9650eecc1b7afbc38a3b5097f827a15baf3fc
James Wolff <james.wolff0@gmail.com>
2024-07-23 01:09:52 -0500

    Add pause menu

---
+++ scripts/menus/pause_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if get_tree().paused:
+		show()
+		$VBoxContainer/ResumeButton.grab_focus()
+	else:
+		hide()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
+		if get_tree().paused:
+			show()
+			$VBoxContainer/ResumeButton.grab_focus()
+		else:
+			hide()
+
+func _on_resume_button_pressed():
+	get_tree().paused = false
+	hide()
+
+
+func _on_options_button_pressed():
+	var options = load("res://scenes/menus/options.tscn").instantiate()
+	add_child(options)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
