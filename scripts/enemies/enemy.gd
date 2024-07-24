4578cb42a5ebfe46529c1f7a8c1d9adf403aea18
James Wolff <james.wolff0@gmail.com>
2024-07-23 22:48:31 -0500

    Add automatic navigation to enemy script

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -1,18 +1,22 @@
-extends StaticBody2D
+extends CharacterBody2D
 
-@export var speed: int = 100
+@export var max_speed: int = 100
+@export var acceleration: int = 500
 @export var total_power: int = 375
+var target: Node2D
 var red: ColorValue
 var green: ColorValue
 var blue: ColorValue
 
 var point_light: PointLight2D
 var animation_controller: AnimatedSprite2D
+var navigation_agent: NavigationAgent2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	point_light = $PointLight2D
 	animation_controller = $AnimatedSprite2D
+	navigation_agent = $NavigationAgent2D
 	var power_remaining = total_power
 	# Initialize the color values
 	red = ColorValue.new(floor(randf_range(0, power_remaining)))
@@ -27,6 +31,11 @@ func _process(_delta):
 	var color = Color(red.normalize(), green.normalize(), blue.normalize())
 	point_light.color = color
 	animation_controller.self_modulate = color
+
+func _physics_process(delta):
+	var direction = to_local(navigation_agent.get_next_path_position()).normalized()
+	velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	move_and_slide()
 	
 func take_damage(damage: int):
 	var power_remaining = red.value + green.value + blue.value
@@ -53,3 +62,15 @@ func take_damage(damage: int):
 	# Repeat the process if we have damage remaining
 	if damage_remaining > 0:
 		take_damage(damage_remaining)
+	
+func set_target(tgt):
+	target = tgt
+	create_path()
+
+func create_path():
+	if not target:
+		return
+	navigation_agent.target_position = target.global_position
+
+func _on_timer_timeout():
+	create_path()
