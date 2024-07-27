e47ef71ebacca804590623e4928b5ad0aecdebb7
James Wolff <james.wolff0@gmail.com>
2024-07-27 10:02:19 -0500

    Add idle animation to shadow and code to enable it

--- scripts/items/shadow.gd
+++ scripts/items/shadow.gd
@@ -1,4 +1,11 @@
 extends RigidBody2D
 
-func _physics_process(delta):
-	pass
+func _on_body_entered(body):
+	if body.has_method("collect"):
+		body.collect(self)
+		queue_free()
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "death":
+		$AnimatedSprite2D.animation = "idle"
+		$AnimatedSprite2D.play()
