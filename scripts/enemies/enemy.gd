a6875a6662f7982d2a326082214a19ed34fce49f
James Wolff <james.wolff0@gmail.com>
2024-07-29 00:33:11 -0500

    Add aura pulsing to enemies and enable damaging the player

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -7,14 +7,22 @@ var target: Node2D
 var red: ColorValue
 var green: ColorValue
 var blue: ColorValue
+var base_aura_energy: float
+var pulsed: bool
+var touching_player: bool
 
-var point_light: PointLight2D
+var aura: PointLight2D
+var aura_pulse_timer: Timer
 var animation_controller: AnimatedSprite2D
 var navigation_agent: NavigationAgent2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	point_light = $PointLight2D
+	pulsed = false
+	touching_player = false
+	aura = $PointLight2D
+	base_aura_energy = aura.energy
+	aura_pulse_timer = $AuraPulseTimer
 	animation_controller = $AnimatedSprite2D
 	navigation_agent = $NavigationAgent2D
 	var power_remaining = total_power
@@ -28,8 +36,11 @@ func _ready():
 	
 	# Set the color of the point light
 	var color = Color(red.normalize(), green.normalize(), blue.normalize())
-	point_light.color = color
+	aura.color = color
 	animation_controller.self_modulate = color
+
+func _process(_delta):
+	update_aura_strength()
 
 func _physics_process(delta):
 	if navigation_agent.is_navigation_finished():
@@ -74,14 +85,52 @@ func create_path():
 	navigation_agent.target_position = target.global_position
 
 func get_color() -> Color:
-	return point_light.color
+	return aura.color
 
 func set_color(r: ColorValue, g: ColorValue, b: ColorValue):
 	red = r
 	green = g
 	blue = b
-	point_light.color = Color(red.normalize(), green.normalize(), blue.normalize())
-	animation_controller.self_modulate = point_light.color
+	aura.color = Color(red.normalize(), green.normalize(), blue.normalize())
+	animation_controller.self_modulate = aura.color
+
+func update_aura_strength():
+	if not aura:
+		return
+	var timer_percentage = aura_pulse_timer.time_left / aura_pulse_timer.wait_time
+	timer_percentage = remap(timer_percentage, 1, 0, -1, .1)
+	var f_a = 1 / pow(timer_percentage, 2)
+	var f_b = -1000 * pow(timer_percentage + .1, 2) + 40
+	var pulse = f_a
+	if timer_percentage >= -0.168192:
+		pulse = f_b
+	pulse = remap(pulse, 0, 40, 0.25, 1.25)
+	if timer_percentage > -0.1 and not pulsed:
+		pulsed = true
+		pulse_aura()
+	aura.energy = base_aura_energy * pulse
+
+func pulse_aura():
+	damage_player()
+
+func damage_player():
+	if not touching_player:
+		return
+	get_tree().call_group("player", "take_damage", 0.5)
 
 func _on_timer_timeout():
 	create_path()
+
+
+func _on_aura_pulse_timer_timeout():
+	pulsed = false
+
+
+func _on_area_2d_body_entered(body:Node2D):
+	if body.is_in_group("player"):
+		touching_player = true
+
+
+func _on_area_2d_body_exited(body:Node2D):
+	if body.is_in_group("player"):
+		touching_player = false
