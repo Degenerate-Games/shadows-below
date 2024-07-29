59dcf8408b6812e685e4f41245faf13ad0b6eea6
James Wolff <james.wolff0@gmail.com>
2024-07-29 00:10:51 -0500

    Add ability to enter doors

--- scripts/doors/door.gd
+++ scripts/doors/door.gd
@@ -4,7 +4,23 @@ var connected_room: Node2D
 
 func enter():
 	if not connected_room:
-		return
+		connected_room = get_parent().get_parent().generate_room()
+	connected_room.show()
+	get_parent().hide()
+	var player = get_tree().get_first_node_in_group("player")
+	match name:
+		"West Door":
+			player.global_position = get_tree().get_nodes_in_group("player_spawn").bsearch_custom("East Spawn", func (a, b) -> int: return a.name.compare_to(b))
+		"East Door":
+			player.global_position = get_tree().get_nodes_in_group("player_spawn").bsearch_custom("West Spawn", func (a, b) -> int: return a.name.compare_to(b))
+		"North Door":
+			player.global_position = get_tree().get_nodes_in_group("player_spawn").bsearch_custom("South Spawn", func (a, b) -> int: return a.name.compare_to(b))
+		"South Door":
+			player.global_position = get_tree().get_nodes_in_group("player_spawn").bsearch_custom("North Spawn", func (a, b) -> int: return a.name.compare_to(b))
 
 func _on_room_complete():
 	$CollisionShape2D.disabled = false
+
+func _on_body_entered(body:Node2D):
+	if body.is_in_group("player"):
+		enter()
