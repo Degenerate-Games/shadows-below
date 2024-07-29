8521be0055f99824c9542313b2b9b359d35a79c2
James Wolff <james.wolff0@gmail.com>
2024-07-29 11:03:10 -0500

    More sanity checks in bake_after

--- scripts/room.gd
+++ scripts/room.gd
@@ -16,14 +16,16 @@ func remove_key(key: Node2D):
 func _on_key_unlocked():
   if keys.all(func (key: Node2D) : return key.unlocked):
     room_complete.emit()
-  else:
-    var locked_keys = keys.filter(func (key: Node2D) : return not key.unlocked)
-    var locked_ratio = locked_keys.size() / keys.size()
-    # set door graphics based on locked_ratio
+  # else:
+	# var locked_keys = keys.filter(func (key: Node2D) : return not key.unlocked)
+	# var locked_ratio = locked_keys.size() / keys.size()
+	# set door graphics based on locked_ratio
 
 func bake_after(frames: int):
   if waiting_to_bake:
     return
+  if get_tree() == null:
+    call_deferred("bake_after", frames)
   waiting_to_bake = true
   await get_tree().create_timer(frames / Engine.max_fps).timeout
   Global.get_first_child_in_group(self, "navigation_region").bake_navigation_polygon()
