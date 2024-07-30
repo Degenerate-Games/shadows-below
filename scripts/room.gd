dfbd6391114cb02b5db32ddd61046c132f024e88
James Wolff <james.wolff0@gmail.com>
2024-07-29 19:49:17 -0500

    Remove unneeded code from room

--- scripts/room.gd
+++ scripts/room.gd
@@ -16,10 +16,6 @@ func remove_key(key: Node2D):
 func _on_key_unlocked():
   if keys.all(func (key: Node2D) : return key.unlocked):
     room_complete.emit()
-  # else:
-	# var locked_keys = keys.filter(func (key: Node2D) : return not key.unlocked)
-	# var locked_ratio = locked_keys.size() / keys.size()
-	# set door graphics based on locked_ratio
 
 func bake_after(frames: int):
   if waiting_to_bake:
