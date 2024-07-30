3396242684d0a3b0e32ba736b415665b2eb6dd8b
James Wolff <james.wolff0@gmail.com>
2024-07-29 20:41:03 -0500

    Minor updates to door script

--- scripts/doors/door.gd
+++ scripts/doors/door.gd
@@ -10,8 +10,8 @@ func enter():
 		connected_room.pack(get_parent().get_parent().generate_room())
 	if not room:
 		room = connected_room.instantiate()
+	get_parent().get_parent().call_deferred("add_child", room)
 	get_parent().queue_free()
-	get_parent().get_parent().add_child(room)
 	var connected_door = null
 	await room.ready
 	match name:
@@ -29,21 +29,21 @@ func enter():
 	var player = get_tree().get_first_node_in_group("player")
 	match name:
 		"West Door":
-			player.global_position = get_spawn_position("East Spawn")
+			player.global_position = get_spawn_position(room, "East Spawn")
 		"East Door":
-			player.global_position = get_spawn_position("West Spawn")
+			player.global_position = get_spawn_position(room, "West Spawn")
 		"North Door":
-			player.global_position = get_spawn_position("South Spawn")
+			player.global_position = get_spawn_position(room, "South Spawn")
 		"South Door":
-			player.global_position = get_spawn_position("North Spawn")
+			player.global_position = get_spawn_position(room, "North Spawn")
 
 func _on_room_complete():
 	print("Room Complete")
 	$Sprite2D.texture = load("res://assets/sprites/doors/temp_door_open.png")
 	$CollisionShape2D.disabled = false
 
-func get_spawn_position(spawn_name: String) -> Vector2:
-	for child in connected_room.get_children():
+func get_spawn_position(room: Node2D, spawn_name: String) -> Vector2:
+	for child in room.get_children():
 		if child.is_in_group("player_spawn") and child.name == spawn_name:
 			return child.global_position
 	return Vector2()
