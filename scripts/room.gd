a3adb38ba7b793b4e1f2a408ae907a376102952e
James Wolff <james.wolff0@gmail.com>
2024-07-29 20:40:24 -0500

    Move navigation baking to _ready

--- scripts/room.gd
+++ scripts/room.gd
@@ -5,6 +5,9 @@ var waiting_to_bake: bool = false
 
 signal room_complete
 
+func _ready():
+  Global.get_first_child_in_group(self, "navigation_region").bake_navigation_polygon()
+
 func add_key(key: Node2D):
   keys.append(key)
   add_child(key)
@@ -16,13 +19,3 @@ func remove_key(key: Node2D):
 func _on_key_unlocked():
   if keys.all(func (key: Node2D) : return key.unlocked):
     room_complete.emit()
-
-func bake_after(frames: int):
-  if waiting_to_bake:
-    return
-  if get_tree() == null:
-    call_deferred("bake_after", frames)
-  waiting_to_bake = true
-  await get_tree().create_timer(frames / Engine.max_fps).timeout
-  Global.get_first_child_in_group(self, "navigation_region").bake_navigation_polygon()
-  waiting_to_bake = false
